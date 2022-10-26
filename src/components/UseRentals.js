dac84185d54c48c970b8ac6f6a84ef76320a9347
Miles College <hbcuc2-miles@Miless-MacBook-Pro.local>
2022-10-26 12:11:41 -0500

    Changed from Zillow Api to USRealEstate api

--- src/components/UseRentals.js
+++ src/components/UseRentals.js
@@ -1,18 +1,6 @@
 import { useState, useEffect } from "react";
 import distance from "./Distance";
-import axios, * as others from 'axios';
 
-// const axios = require("axios");
-
-const options = {
-    method: 'GET',
-    url: 'https://zillow-com1.p.rapidapi.com/similarForRent',
-    params: {zpid: '77224'},
-    headers: {
-      'X-RapidAPI-Key': '1ef3fb1bfcmsh6baad8915fcf34fp187976jsn3aa75fb14015',
-      'X-RapidAPI-Host': 'zillow-com1.p.rapidapi.com'
-    }
-  };
   
 
 
@@ -21,24 +9,29 @@ const useRentals = (preferredLocation) => {
     const [zillow, setZillow] = useState([]);
     const [locations, setLocations] = useState([]);
     useEffect(()=> {
-        axios.request(options).then(function (response) {
-        //    response.map((feature) => {
-        //     if (distance(preferredLocation[1], preferredLocation[0], feature.latitude, feature.longitude) <= 30) {
-        //         setZillow([...zillow, [feature.longitude, feature.latitude]])
-        //         setLocations([...locations, feature])
-        //     }
 
-        //    })
-        console.log(response);
-        }).catch(function (error) {
-            console.error(error);
-        });
-    }, [])
+     fetch("https://us-real-estate.p.rapidapi.com/for-rent",  {
+            'method': 'GET',
+            'headers': {
+                'X-RapidAPI-Key': '1ef3fb1bfcmsh6baad8915fcf34fp187976jsn3aa75fb14015',
+                'X-RapidAPI-Host': 'us-real-estate.p.rapidapi.com'
+            }}).then((response)=> response.json())
+               .then((result) => {
+                   result.data.results.map((feature) => {
+                       if(distance(preferredLocation[1], preferredLocation[0], feature.location.address.coordinate.lat, feature.location.address.coordinate.lng) <= 30){
+                           setZillow([...zillow, [feature.location.address.coordinate.lng, feature.location.address.coordinate.lat]]);
+                       }});
+                // console.log(result);
+                   
+               }).catch ((err)=> {console.log(err);} );
+     
+    }, []);
 
     return {
         locations,
         setLocations,
-        zillow, setZillow
+        zillow, 
+        setZillow,
     }
 }
 
